2f617fe8556c20fbc3d79f4074a9c5e63ab2b3fb
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-13 10:38:48 +0530

    added react card project

---
+++ Cards-Project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from "./components/Card"
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ Cards-Project/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import BookmarkPlus from "lucide-react"
+const Card = () => {
+  return (
+    <>
+    <div className="card">
+      <h3>Liam Payne</h3>
+      <img
+        src="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bWVufGVufDB8fDB8fHww"
+        alt=""
+      />
+      <button>View Profile</button>
+    </div>
+    
+    </>
+  );
+};
+
+export default Card;
