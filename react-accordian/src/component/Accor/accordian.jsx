08dd8311f024a26f0c5bcd4f0755ea9ca74c56a3
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-21 16:19:25 +0530

    completed accordian project

--- react-accordian/src/component/Accor/accordian.jsx
+++ react-accordian/src/component/Accor/accordian.jsx
@@ -3,25 +3,62 @@ import data from "./data";
 
 const Accordian = () => {
   const [selected, setselected] = useState(null);
+  const [enableMultipleSelection, setEnableMultipleSelection] = useState(false);
+  const [multiple, setmultiple] = useState([]);
 
   const handleaccordian = (currtId) => {
-    setselected(selected === currtId ?null:currtId);
+    setselected(selected === currtId ? null : currtId);
   };
+
+  const handleMultipleSelection = (currtId) => {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(currtId);
+
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(currtId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+    setmultiple(cpyMultiple);
+  };
+  console.log(selected, multiple);
 
   return (
     <div>
-      <div className="main">
-        <div className="accordian">
+      <div className="main flex justify-center items-center flex-col bg-teal-600 h-screen">
+        <button
+          onClick={() => setEnableMultipleSelection(!enableMultipleSelection)}
+          className="border p-2 text-3xl font font-semibold bg-black text-gray-600"
+        >
+          Enable Multi Select
+        </button>
+        <div className="accordian text-2xl w-[50vw]">
           {data && data.length > 0 ? (
             data.map((dataItem) => (
-              <div key={dataItem.id} className="item">
+              <div key={dataItem.id} className="item p-3 bg-amber-300">
                 <div
-                  onClick={() => handleaccordian(dataItem.id)}
-                  className="title"
+                  onClick={
+                    enableMultipleSelection
+                      ? () => handleMultipleSelection(dataItem.id)
+                      : () => handleaccordian(dataItem.id)
+                  }
+                  className="title text-3xl  font-extrabold flex justify-between bg-orange-800"
                 >
                   {dataItem.question} <span>+</span>
                 </div>
-                {selected === dataItem.id ? <div>{dataItem.answer}</div> : null}
+                {enableMultipleSelection
+                  ? multiple.indexOf(dataItem.id) !== -1 && (
+                      <div className="text-amber-200 bg-orange-800 text-center">
+                        {dataItem.answer}
+                      </div>
+                    )
+                  : selected === dataItem.id && (
+                      <div className="text-amber-200 bg-orange-800 text-center">
+                        {dataItem.answer}
+                      </div>
+                    )}
+                {selected === dataItem.id ? (
+                  <div className="text-amber-200 bg-orange-800 text-center">
+                    {dataItem.answer}
+                  </div>
+                ) : null}
               </div>
             ))
           ) : (
