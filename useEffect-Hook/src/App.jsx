789dedd6314fcccbf5493a5049caac2fdaaa429f
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-26 22:45:00 +0530

    added new project folder Gallery project

--- useEffect-Hook/src/App.jsx
+++ useEffect-Hook/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [num, setnum] = useState(0)
+  const [num2, setnum2] = useState(100)
+  useEffect(()=>{
+    console.log("use effect running....");
+    
+  },[])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+  return <div>
+    <h1>num is :{num}</h1>
+    <h2>num2:{num2}</h2>
+    <button onClick={()=>{
+      setnum(num+1)
+      setnum2(num2+10)
+    }}>Increment</button>
+  </div>;
+};
+
+export default App;
