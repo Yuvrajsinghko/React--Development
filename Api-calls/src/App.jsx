2aba37d1d6563ebb346968ef278f7bd0bcf1e385
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-26 11:08:30 +0530

    copleted api concept implementation

--- Api-calls/src/App.jsx
+++ Api-calls/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [imageArr, setImageArr] = useState([]);
+  const getData = async () => {
+    // const {data} = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    // const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    // const data = await response.json();
+    // console.log(data);
+
+    const { data } = await axios("https://picsum.photos/v2/list");
+    setImageArr(data);
+  };
 
   return (
-    <>
+    <div>
+      <button onClick={getData}>Get Data</button>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {imageArr.map((elem, idx) => (
+         <div key={idx}>
+          <img src={elem.download_url}alt="hehe" />
+         </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
