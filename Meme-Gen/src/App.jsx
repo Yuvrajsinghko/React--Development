7b4dcd9dc752b429616ed6512300b33af34b23b9
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-12-28 14:57:48 +0530

    installed redux toolkit,axios for handling apis,tailwindcss for styling

--- Meme-Gen/src/App.jsx
+++ Meme-Gen/src/App.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { fetchGIFs, fetchPhotos, fetchVideos } from "./api/mediaApi";
 
 const App = () => {
+  const getPhotos = () => {};
   return (
-    <div>App</div>
-  )
-}
+    <div className="h-screen w-full text-teal-500 text-7xl bg-gray-950">
+      <button
+        className="border-2"
+        onClick={ async() => {
+          const data = await fetchPhotos("dog");
+          console.log(data.results);
+          
+        }}
+      >
+        Get Photos
+      </button>
+      <button
+        className="border-2 mt-5"
+        onClick={ async() => {
+          const data = await fetchVideos("dog");
+          console.log(data.videos);
+          
+        }}
+      >
+        Get Videos
+      </button>
+      <button
+        className="border-2 mt-5"
+        onClick={ async() => {
+          const data = await fetchGIFs("dog");
+          console.log(data.results);
+          
+        }}
+      >
+        Get GIFs
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
