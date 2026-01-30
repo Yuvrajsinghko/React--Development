568b5be019919f8d0b10b6c8d760debfb78410d9
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-01-30 22:50:38 +0530

    added some extra code to it

--- Forms-react/src/App.jsx
+++ Forms-react/src/App.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import DecryptedText from "./component/DecryptedText";
+import GlitchText from "./component/GlitchText";
 
 const App = () => {
-  let submitHandler=(e)=>{
-    e.preventDefault()
+  let submitHandler = (e) => {
+    e.preventDefault();
     console.log("submit Handler has triggered");
-    
-  }
-  let handleChange=(event)=>{
+  };
+  let handleChange = (event) => {
     console.log(event.target);
-    
-
-  }
+  };
   return (
-<div>
-  <form onSubmit={(e)=>{
-    submitHandler(e)
-  }} >
-    <input type="text" placeholder='Enter you name' onChange={(event)=>handleChange(event)} />
-    <br />
-    <button>Submit</button>
-  </form>
-</div>  )
-}
+    <div>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter you name"
+          onChange={(event) => handleChange(event)}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
 
-export default App
+      <DecryptedText
+      
+        text="HII RAGNAR"
+        speed={80}
+        maxIterations={10}
+        characters="ABCD1234!?"
+        className="revealed"
+        parentClassName="all-letters text-6xl text-red-400 font-bold "
+        encryptedClassName="encrypted"
+        sequential="false"
+      />
+    </div>
+  );
+};
+
+export default App;
